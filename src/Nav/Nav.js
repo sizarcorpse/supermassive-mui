fc56cc45e2ba2dd066c68d8b622f85083d27ff91
sizar corpse <sizarcorpse@gmail.com>
2020-12-25 05:01:34 +0600

    nav

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -15,11 +15,24 @@ import {
   Box,
   Hidden,
   SvgIcon,
-  Tooltip
+  Tooltip,
+  MenuItem,
+  Menu,
+  Avatar
 } from "@material-ui/core";
 
 const Nav = (props) => {
   const { classes } = props;
+
+  // #action : category menu
+  const [categoryMenuOpen, setCategoryMenuOpen] = useState(null);
+
+  const handleCategoryMenuOpen = (event) => {
+    setCategoryMenuOpen(event.currentTarget);
+  };
+  const handleCategoryMenuClose = () => {
+    setCategoryMenuOpen(null);
+  };
 
   return (
     <Grid container component="main">
@@ -31,32 +44,195 @@ const Nav = (props) => {
           className={classes.scui_nav_appbar}
         >
           <Toolbar className={classes.scui_nav_toolbar}>
-            <Box display="flex" justifyContent="center" alignItems="center">
-              <Box></Box>
-              <Box display="flex">
-                {[
-                  "Home",
-                  "Categories",
-                  "Post by Reaction",
-                  "Archive",
-                  "Sample Page",
-                  "Contat Form"
-                ].map((m, i) => (
-                  <Box key={i} m={2}>
-                    <Link to="#" className={classes.scui_link_underline_remove}>
-                      <Typography
-                        variant="h2"
-                        color="primary"
+            <Grid item xs={4} sm={4} md={3} lg={2} xl={2}>
+              <Box display="flex" justifyContent="center">
+                <Typography variant="h6" color="primary">
+                  suppermassive
+                </Typography>
+              </Box>
+            </Grid>
+
+            <Grid item xs={2} sm={4} md={6} lg={8} xl={8}>
+              <Hidden mdDown>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Box display="flex">
+                    <Box m={2}>
+                      <Link
+                        to="#"
+                        className={classes.scui_link_underline_remove}
+                      >
+                        <Typography
+                          variant="h2"
+                          color="primary"
+                          className={classes.scui_nav_onhover}
+                        >
+                          Home
+                        </Typography>
+                      </Link>
+                    </Box>
+                    <Box m={2}>
+                      <Link
+                        to="#"
+                        onClick={handleCategoryMenuOpen}
+                        className={classes.scui_link_underline_remove}
+                      >
+                        <Typography
+                          variant="h2"
+                          color="primary"
+                          className={classes.scui_nav_onhover}
+                        >
+                          Categories
+                        </Typography>
+                      </Link>
+                    </Box>
+                    <Menu
+                      id="simple-menu"
+                      className={classes.scui_nav_menu}
+                      anchorEl={categoryMenuOpen}
+                      keepMounted
+                      open={Boolean(categoryMenuOpen)}
+                      onClose={handleCategoryMenuClose}
+                    >
+                      <MenuItem
+                        onClick={handleCategoryMenuClose}
                         className={classes.scui_nav_onhover}
                       >
-                        {m}
-                      </Typography>
-                    </Link>
+                        <Typography
+                          variant="h3"
+                          color="primary"
+                          className={classes.scui_nav_onhover}
+                        >
+                          Funny News
+                        </Typography>
+                      </MenuItem>
+                      <MenuItem
+                        onClick={handleCategoryMenuClose}
+                        className={classes.scui_nav_onhover}
+                      >
+                        <Typography
+                          variant="h3"
+                          color="primary"
+                          className={classes.scui_nav_onhover}
+                        >
+                          Viral
+                        </Typography>
+                      </MenuItem>
+                      <MenuItem
+                        onClick={handleCategoryMenuClose}
+                        className={classes.scui_nav_onhover}
+                      >
+                        <Typography
+                          variant="h3"
+                          color="primary"
+                          className={classes.scui_nav_onhover}
+                        >
+                          Featured
+                        </Typography>
+                      </MenuItem>
+                      <MenuItem
+                        onClick={handleCategoryMenuClose}
+                        className={classes.scui_nav_onhover}
+                      >
+                        <Typography
+                          variant="h3"
+                          color="primary"
+                          className={classes.scui_nav_onhover}
+                        >
+                          Gallery
+                        </Typography>
+                      </MenuItem>
+                    </Menu>
+                    <Box m={2}>
+                      <Link
+                        to="#"
+                        className={classes.scui_link_underline_remove}
+                      >
+                        <Typography
+                          variant="h2"
+                          color="primary"
+                          className={classes.scui_nav_onhover}
+                        >
+                          Post by Reaction
+                        </Typography>
+                      </Link>
+                    </Box>
+                    <Box m={2}>
+                      <Link
+                        to="#"
+                        className={classes.scui_link_underline_remove}
+                      >
+                        <Typography
+                          variant="h2"
+                          color="primary"
+                          className={classes.scui_nav_onhover}
+                        >
+                          Archive
+                        </Typography>
+                      </Link>
+                    </Box>
+                    <Box m={2}>
+                      <Link
+                        to="#"
+                        className={classes.scui_link_underline_remove}
+                      >
+                        <Typography
+                          variant="h2"
+                          color="primary"
+                          className={classes.scui_nav_onhover}
+                        >
+                          Sample Page
+                        </Typography>
+                      </Link>
+                    </Box>
+                    <Box m={2}>
+                      <Link
+                        to="#"
+                        className={classes.scui_link_underline_remove}
+                      >
+                        <Typography
+                          variant="h2"
+                          color="primary"
+                          className={classes.scui_nav_onhover}
+                        >
+                          Contat Form
+                        </Typography>
+                      </Link>
+                    </Box>
                   </Box>
-                ))}
+                </Box>
+              </Hidden>
+              <Box
+                display={{
+                  xs: "flex",
+                  sm: "flex",
+                  md: "flex",
+                  lg: "none",
+                  xl: "none"
+                }}
+                justifyContent="flex-end"
+              >
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="flex-end"
+                >
+                  <Avatar> </Avatar>
+                </Box>
               </Box>
-              <Box></Box>
-            </Box>
+            </Grid>
+
+            <Grid item xs={4} sm={4} md={3} lg={2} xl={2}>
+              <Box display="flex" justifyContent="center" alignItems="center">
+                <Box display="flex" alignItems="center" mx={2}>
+                  <Avatar> </Avatar>
+                </Box>
+                <Box display="flex" alignItems="center">
+                  <Typography variant="h1" color="primary">
+                    sign in | sign up
+                  </Typography>
+                </Box>
+              </Box>
+            </Grid>
           </Toolbar>
         </AppBar>
       </Grid>
